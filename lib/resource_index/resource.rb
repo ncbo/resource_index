579ad594a12cb3e6d53d075ae3e69ea574b29efa
Paul R Alexander <palexander@stanford.edu>
2014-05-15 16:38:54 -0700

    Method to get documents

--- lib/resource_index/resource.rb
+++ lib/resource_index/resource.rb
@@ -30,10 +30,47 @@ module ResourceIndex
       doc.elements.to_a("//ontoIds/entry").select {|a| !a.elements["string[position() = 2]"].text.eql?("null")}.map {|a| fields[a.elements["string[position() = 1]"].text].ontology = a.elements["string[position() = 2]"].text.to_i}
     end
 
-    # get fields from structure
-    # include name, related ontologies and weights
+    ##
+    # Get fields from structure
+    # Include name, related ontologies and weights
     def fields
       @fields
+    end
+
+    ##
+    # Return a lazy enumerator that will lazily get results from the DB
+    def documents(chunk_size: 5000)
+      unless RI::Document.const_defined?(self.acronym)
+        fields = self.fields.keys.map {|f| f.to_sym}
+        cls = Class.new(RI::Document) do
+          fields.each do |field|
+            define_method field do
+              instance_variable_get("@#{field.downcase}")
+            end
+            define_method "#{field.downcase}=".to_sym do |arg|
+              instance_variable_set("@#{field.downcase}", arg)
+            end
+          end
+        end
+        cls.define_singleton_method :from_hash do |hsh|
+          inst = self.new
+          hsh.each {|k,v| inst.send("#{k}=", v)}
+          inst
+        end
+        RI::Document.const_set(self.acronym, cls)
+      end
+      cls ||= RI::Document.const_get(self.acronym)
+      return Enumerator.new { |yielder|
+        offset = 0
+        docs = nil
+        while docs.nil? || docs.length > 0
+          docs = RI.db["obr_#{self.acronym.downcase}_element".to_sym].limit(chunk_size).offset(offset).all
+          docs.each do |doc|
+            yielder << cls.from_hash(doc) if doc
+          end
+          offset += chunk_size
+        end
+      }.lazy
     end
   end
 end
