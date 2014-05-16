d8071b2499afa20e2078fd4934c30a102e85f7b7
Paul R Alexander <palexander@stanford.edu>
2014-05-16 10:54:24 -0700

    Methods for indexing/annotating document

--- lib/resource_index/document.rb
+++ lib/resource_index/document.rb
@@ -2,4 +2,16 @@ class RI::Document
   attr_accessor :id, :document_id, :dictionary_id, :resource
   alias :local_element_id :document_id
   alias :"local_element_id=" :"document_id="
+
+  def indexable_hash
+    fields = RI::Resource.find(self.resource).fields.keys.map {|f| f.downcase.to_sym}
+    hash = {}
+    fields.each {|f| hash[f] = self.send(f)}
+    hash
+  end
+
+  def annotatable_text
+    fields = RI::Resource.find(self.resource).fields.keys.map {|f| f.downcase.to_sym}
+    fields.map {|f| self.send(f)}.join("\n\n")
+  end
 end
