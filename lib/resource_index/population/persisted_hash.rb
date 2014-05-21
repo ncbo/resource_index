90d67591e58e8b59c85a157bf5902d2cf959bba3
Paul R Alexander <palexander@stanford.edu>
2014-05-21 10:27:11 -0700

    A hash that gets automatically persisted to disk

---
+++ lib/resource_index/population/persisted_hash.rb
@@ -0,0 +1,98 @@
+require 'forwardable'
+require 'zlib'
+
+module Persisted
+  class Hash
+    extend Forwardable
+
+    class CopyError < StandardError; end
+    class ExistsError < StandardError; end
+
+    def_delegators :@hash, *(::Hash.public_methods(false) - [:"[]=", :merge, :"merge!", :invert])
+
+    def initialize(name,
+                   persist_write_count: 10000,
+                   gzip: false,
+                   hash: nil,
+                   dir: nil)
+      @hash = hash || {}
+      @write_count = 0
+      @dir ||= Dir.pwd
+      path = @dir + "/#{name}_persisted_hash.dump"
+      @base_path = File.expand_path(path)
+      @persist_write_count = persist_write_count
+      @gzip = gzip
+      if File.exist?(path)
+        load
+      end
+      at_exit do
+        write
+      end
+    end
+
+    def write
+      if @gzip
+        File.open(path, 'w') do |f|
+          gz = Zlib::GzipWriter.new(f)
+          gz.write Marshal.dump(@hash)
+          gz.close
+        end
+      else
+        File.open(path, 'w') do |f|
+          f.write Marshal.dump(@hash)
+          f.close
+        end
+      end
+    end
+
+    def []=(key, value)
+      count_writes
+      ret_val = @hash[key] = value
+      write_check
+      ret_val
+    end
+
+    def merge!(hash)
+      count_writes(hash.keys.length)
+      ret_val = @hash.merge!(hash)
+      write_check
+      ret_val
+    end
+
+    ## Don't allow copying
+    def nodup(*args)
+      raise CopyError, "#{self.class.name} cannot be copied, duplicated or cloned"
+    end
+    alias :invert :nodup
+    alias :merge :nodup
+    alias :dup :nodup
+    alias :clone :nodup
+
+    private
+
+    def path
+      @base_path + (@gzip ? ".gz" : "")
+    end
+
+    def count_writes(num = 1)
+      @write_count += num
+    end
+
+    def write_check
+      if @write_count >= @persist_write_count
+        write
+        @write_count = 0
+      end
+    end
+
+    def load
+      if @gzip
+        Zlib::GzipReader.open(path) do |gz|
+          @hash = Marshal.load(gz.read)
+        end
+      else
+        @hash = Marshal.load(File.read(path))
+      end
+    end
+  end
+end
