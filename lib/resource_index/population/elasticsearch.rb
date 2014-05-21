9234d853965688da8d20e6c01a03539ad516b11e
Paul R Alexander <palexander@stanford.edu>
2014-05-21 10:35:55 -0700

    Refactor out elasticsearch code

---
+++ lib/resource_index/population/elasticsearch.rb
@@ -0,0 +1,88 @@
+require 'elasticsearch'
+
+module RI::Population::Elasticsearch
+  def index_id
+    "#{@res.acronym.downcase}_#{@time.to_i}"
+  end
+
+  def index_documents
+    count = 0
+    @res.documents.each do |doc|
+      annotations = {}
+      annotated_classes(doc).each do |cls|
+        if annotations[cls.xxhash]
+          annotations[cls.xxhash][:count] += 1
+          next
+        end
+
+        ancestors = nil
+        @@mutex.synchronize { ancestors = ancestors_hash[cls.xxhash] }
+        unless ancestors
+          submission_id = "http://data.bioontology.org/ontologies/#{cls.ont_acronym}/submissions/#{latest_submissions[cls.ont_acronym]}"
+          ancestors = cls.retrieve_ancestors(cls.ont_acronym, submission_id)
+          @@mutex.synchronize { ancestors_hash[cls.xxhash] = ancestors }
+        end
+        annotations[cls.xxhash] = {direct: cls.xxhash, ancestors: ancestors, count: 1}
+      end
+
+      # Switch the annotaions to an array
+      index_doc = doc.indexable_hash
+      index_doc[:annotations] = annotations.values
+
+      # Add to batch index, push to ES if we hit the chunk size limit
+      @mutex.synchronize {
+        @es_queue << doc
+
+        if @es_queue.length >= settings.bulk_index_size
+          @logger.debug "Indexing docs"
+          store_documents
+        end
+      }
+      @logger.debug "Doc count: #{count += 1}"
+    end
+  end
+
+  def create_index
+    @es.indices.create index: index_id, type: @res.acronym, body: es_mapping
+  end
+
+  def alias_index
+    @es.indices.put_alias index: index_id, name: @res.acronym
+  end
+
+  def store_documents
+    bulk_items = []
+    @es_queue.each do |doc|
+      bulk_items << {index: {_index: index_id, _type: @res.acronym, _id: doc.id, data: doc}}
+    end
+    es.bulk body: bulk_queue
+    @es_queue = {}
+  end
+
+  def es_mapping
+    properties_json = Hash[@res.fields.values.map {|f| [f.name.to_sym, {type: :string}] }]
+    {
+      mappings: {
+        @res.acronym => {
+          :"_source" => {
+            includes: @res.fields.values.map {|f| f.name.to_sym},
+            excludes: [:annotations]
+          },
+          properties: properties_json.merge(ANNOTATIONS_MAPPING)
+        }
+      }
+    }
+  end
+
+ANNOTATIONS_MAPPING = {
+  annotations: {
+      type: :nested,
+      include_in_all: false,
+      properties: {
+          direct: {type: :long, store: false, include_in_all: false},
+          ancestors: {type: :long, store: false, include_in_all: false},
+          count: {type: :long, store: false, include_in_all: false}
+      }
+  }
+}
+end
--- lib/resource_index/population/population.rb
+++ lib/resource_index/population/population.rb
@@ -1,9 +1,9 @@
 require 'logger'
-require 'elasticsearch'
 require 'ostruct'
 require 'json'
 require 'zlib'
 require_relative 'goo'
+require_relative 'elasticsearch'
 require_relative 'mgrep/mgrep'
 require_relative 'label_converter'
 require_relative 'persisted_hash'
@@ -12,6 +12,7 @@ module RI::Population; end
 
 class RI::Population::Manager
   include RI::Population::Goo
+  include RI::Population::Elasticsearch
 
   ALL_ANCESTORS_FILE = "ancestors.gz"
 
@@ -64,10 +65,6 @@ class RI::Population::Manager
 
   private
 
-  def index_id
-    "#{@res.acronym.downcase}_#{@time.to_i}"
-  end
-
   def latest_submissions
     @latest_submissions ||= latest_submissions_sparql
   end
@@ -96,116 +93,10 @@ class RI::Population::Manager
     @@ancestors
   end
 
-  def index_documents
-    count = 0
-    @res.documents.each do |doc|
-      annotations = {}
-      annotated_classes(doc).each do |cls|
-        if annotations[cls.xxhash]
-          annotations[cls.xxhash][:count] += 1
-          next
-        end
-
-        ancestors = nil
-        @@mutex.synchronize { ancestors = ancestors_hash[cls.xxhash] }
-        unless ancestors
-          submission_id = "http://data.bioontology.org/ontologies/#{cls.ont_acronym}/submissions/#{latest_submissions[cls.ont_acronym]}"
-          ancestors = cls.retrieve_ancestors(cls.ont_acronym, submission_id)
-          @@mutex.synchronize { ancestors_hash[cls.xxhash] = ancestors }
-        end
-        annotations[cls.xxhash] = {direct: cls.xxhash, ancestors: ancestors, count: 1}
-      end
-
-      # Switch the annotaions to an array
-      index_doc = doc.indexable_hash
-      index_doc[:annotations] = annotations.values
-
-      # Add to batch index, push to ES if we hit the chunk size limit
-      @mutex.synchronize {
-        @es_queue << doc
-
-        if @es_queue.length >= settings.bulk_index_size
-          @logger.debug "Indexing docs"
-          store_documents
-        end
-      }
-      @logger.debug "Doc count: #{count += 1}"
-    end
-  end
-
   def annotated_classes(doc)
     annotations = @mgrep.annotate(doc.annotatable_text, false)
     string_ids = Set.new
     annotations.each {|a| string_ids << a.string_id.to_i}
     @label_converter.convert(string_ids)
   end
-
-  def create_index
-    @es.indices.create index: index_id, type: @res.acronym, body: es_mapping
-  end
-
-  def alias_index
-    @es.indices.put_alias index: index_id, name: @res.acronym
-  end
-
-  def store_documents
-    bulk_items = []
-    @es_queue.each do |doc|
-      bulk_items << {index: {_index: index_id, _type: @res.acronym, _id: doc.id, data: doc}}
-    end
-    es.bulk body: bulk_queue
-    @es_queue = {}
-  end
-
-  def es_mapping
-    properties_json = Hash[@res.fields.values.map {|f| [f.name.to_sym, {type: :string}] }]
-    {
-      mappings: {
-        @res.acronym => {
-          :"_source" => {
-            includes: @res.fields.values.map {|f| f.name.to_sym},
-            excludes: [:annotations]
-          },
-          properties: properties_json.merge(ANNOTATIONS_MAPPING)
-        }
-      }
-    }
-  end
-
-ANNOTATIONS_MAPPING = {
-  annotations: {
-      type: :nested,
-      include_in_all: false,
-      properties: {
-          direct: {type: :long, store: false, include_in_all: false},
-          ancestors: {type: :long, store: false, include_in_all: false},
-          count: {type: :long, store: false, include_in_all: false}
-      }
-  }
-}
-
-<<-EOS
-{
-    "mappings": {
-        "%doc_type%": {
-            "_source" : {
-              "includes": ["title", "abstract"],
-              "excludes": ["annotations"]
-            },
-            "properties": {
-                %properties_json%,
-                "annotations": {
-                    "type": "nested",
-                    "include_in_all": false,
-                    "properties": {
-                        "direct": {"type": "long", "store": false, "include_in_all": false},
-                        "ancestors": {"type": "long", "store": false, "include_in_all": false},
-                        "count": {"type": "long", "store": false, "include_in_all": false}
-                    }
-                }
-            }
-        }
-    }
-}
-EOS
 end
