dd2d648ad376ed4e808ccd12c3d30cb2053c1c77
Paul R Alexander <palexander@stanford.edu>
2014-05-21 10:32:57 -0700

    Refactor out Goo-related code

--- lib/resource_index/population/population.rb
+++ lib/resource_index/population/population.rb
@@ -1,9 +1,9 @@
 require 'logger'
-require 'goo'
 require 'elasticsearch'
 require 'ostruct'
 require 'json'
 require 'zlib'
+require_relative 'goo'
 require_relative 'mgrep/mgrep'
 require_relative 'label_converter'
 require_relative 'persisted_hash'
@@ -11,6 +11,8 @@ require_relative 'persisted_hash'
 module RI::Population; end
 
 class RI::Population::Manager
+  include RI::Population::Goo
+
   ALL_ANCESTORS_FILE = "ancestors.gz"
 
   def initialize(res, opts = {})
@@ -168,62 +170,6 @@ class RI::Population::Manager
         }
       }
     }
-  end
-
-  def goo_setup(opts)
-    # Set defaults
-    @settings.goo_port          = opts[:goo_port] || 9000
-    @settings.goo_host          = opts[:goo_host] || "localhost"
-    @settings.search_server_url = opts[:search_server_url] || "http://localhost:8983/solr"
-    @settings.enable_goo_cache  = opts[:enable_goo_cache] || false
-
-    goo_namespaces
-    goo_connect
-  end
-
-  ##
-  # Connect to goo by configuring the store and search server
-  def goo_connect
-    port              ||= @settings.goo_port
-    host              ||= @settings.goo_host
-
-    begin
-      Goo.use_cache = @settings.enable_goo_cache
-      Goo.configure do |conf|
-        conf.queries_debug(@settings.queries_debug)
-        conf.add_sparql_backend(:main, query: "http://#{host}:#{port}/sparql/",
-                                data: "http://#{host}:#{port}/data/",
-                                update: "http://#{host}:#{port}/update/",
-                                options: { rules: :NONE })
-
-        conf.add_search_backend(:main, service: @settings.search_server_url)
-        conf.add_redis_backend(host: @settings.goo_redis_host,
-                               port: @settings.goo_redis_port)
-      end
-    rescue Exception => e
-      abort("EXITING: Cannot connect to triplestore and/or search server:\n  #{e}\n#{e.backtrace.join("\n")}")
-    end
-  end
-
-  ##
-  # Configure ontologies_linked_data namespaces
-  # We do this at initial runtime because goo needs namespaces for its DSL
-  def goo_namespaces
-    Goo.configure do |conf|
-      conf.add_namespace(:omv, RDF::Vocabulary.new("http://omv.ontoware.org/2005/05/ontology#"))
-      conf.add_namespace(:skos, RDF::Vocabulary.new("http://www.w3.org/2004/02/skos/core#"))
-      conf.add_namespace(:owl, RDF::Vocabulary.new("http://www.w3.org/2002/07/owl#"))
-      conf.add_namespace(:rdfs, RDF::Vocabulary.new("http://www.w3.org/2000/01/rdf-schema#"))
-      conf.add_namespace(:metadata, RDF::Vocabulary.new("http://data.bioontology.org/metadata/"), default = true)
-      conf.add_namespace(:metadata_def, RDF::Vocabulary.new("http://data.bioontology.org/metadata/def/"))
-      conf.add_namespace(:dc, RDF::Vocabulary.new("http://purl.org/dc/elements/1.1/"))
-      conf.add_namespace(:xsd, RDF::Vocabulary.new("http://www.w3.org/2001/XMLSchema#"))
-      conf.add_namespace(:oboinowl_gen, RDF::Vocabulary.new("http://www.geneontology.org/formats/oboInOWL#"))
-      conf.add_namespace(:obo_purl, RDF::Vocabulary.new("http://purl.obolibrary.org/obo/"))
-      conf.add_namespace(:umls, RDF::Vocabulary.new("http://bioportal.bioontology.org/ontologies/umls/"))
-      conf.id_prefix = "http://data.bioontology.org/"
-      conf.pluralize_models(true)
-    end
   end
 
 ANNOTATIONS_MAPPING = {
