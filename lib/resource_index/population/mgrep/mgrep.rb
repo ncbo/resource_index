bd0e92cc8a97b9d71cb8e1087b7473f8174e8223
Paul R Alexander <palexander@stanford.edu>
2014-05-27 18:31:40 -0700

    Auto-threaded version of mgrep client

--- lib/resource_index/population/mgrep/mgrep.rb
+++ lib/resource_index/population/mgrep/mgrep.rb
@@ -1,2 +1,3 @@
 require_relative "mgrep_client"
 require_relative "mgrep_annotated_text"
+require_relative "mgrep_threaded"
---
+++ lib/resource_index/population/mgrep/mgrep_threaded.rb
@@ -0,0 +1,29 @@
+require_relative 'mgrep_client'
+
+module Annotator::Mgrep
+  class ThreadedClient
+    def initialize(host, port, threads = 1)
+      @host = host
+      @port = port
+      @pool = []
+      (threads * 1.5).to_i.times do
+        @pool << Annotator::Mgrep::Client.new(@host, @port)
+      end
+      @assigned = {}
+    end
+
+    def close
+      pooled_client.close()
+    end
+
+    def annotate(text, longword, wholeword = nil)
+      pooled_client.annotate(text, longword, wholeword)
+    end
+
+    def pooled_client
+      client = @assigned[Thread.current.object_id]
+      client ||= @assigned[Thread.current.object_id] = Annotator::Mgrep::Client.new(@host, @port)
+      client
+    end
+  end
+end
