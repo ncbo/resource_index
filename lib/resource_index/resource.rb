b549bbde4c07a6b2be0c077ca4a1d297cb778102
Paul R Alexander <palexander@stanford.edu>
2014-06-24 13:23:30 -0700

    More conditions for containing an ontology

--- lib/resource_index/resource.rb
+++ lib/resource_index/resource.rb
@@ -26,7 +26,7 @@ module ResourceIndex
       @fields = {}
       doc.elements.to_a("//contexts/entry/string").each {|a| fields[a.text] = Field.new(a.text.split("_")[1..-1].join("_"))} # Context names, create field obj
       doc.elements.to_a("//weights/entry").map {|a| fields[a.elements["string"].text].weight = a.elements["double"].text.to_f}
-      doc.elements.to_a("//ontoIds/entry").select {|a| !a.elements["string[position() = 2]"].text.eql?("null")}.map {|a| fields[a.elements["string[position() = 1]"].text].ontology = a.elements["string[position() = 2]"].text.to_i}
+      doc.elements.to_a("//ontoIds/entry").select {|a| contain_ont?(a)}.map {|a| fields[a.elements["string[position() = 1]"].text].ontology = a.elements["string[position() = 2]"].text.to_i}
     end
 
     ##
@@ -41,5 +41,12 @@ module ResourceIndex
     def documents(opts = {})
       RI::Document.all(self, opts)
     end
+
+    private
+
+    def contain_ont?(a)
+      !a.elements["string[position() = 2]"].text.eql?("null") && !a.elements["string[position() = 2]"].text.eql?("-1")
+    end
+
   end
 end
