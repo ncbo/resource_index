29eb010df4ae6ffcf445c46afaaa0956ae10fec5
Paul R Alexander <palexander@stanford.edu>
2014-06-25 16:49:53 -0700

    More options for causing errors for testing

--- test/population_setup.rb
+++ test/population_setup.rb
@@ -64,15 +64,25 @@ end
 ##
 # Monkeypatch to simulate failure when indexing
 class RI::Document
-  def self.fail_on_index(bool)
-    @@fail_count    = 0
-    @@fail_on_index = bool
+  def self.fail_on_index(bool, fail_on_count = 300, max_fails = Float::INFINITY)
+    @@fail_count     = 0
+    @@failures       = 0
+    @@fail_on_count = fail_on_count
+    @@fail_on_index  = bool
+    @@fail_count_max = max_fails
   end
 
   alias_method :old_indexable_hash, :indexable_hash
   def indexable_hash(*args)
-    raise StandardError if @@fail_on_index && @@fail_count > 0 && @@fail_count % 300 == 0
+    if fail?
+      @@failures += 1
+      raise StandardError
+    end
     @@fail_count += 1
     old_indexable_hash(*args)
+  end
+
+  def fail?
+    @@fail_on_index && @@fail_count > 0 && @@fail_count % @@fail_on_count == 0 && @@failures <= @@fail_count_max
   end
 end
