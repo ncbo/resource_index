aa23ef7465b2b4fb0ce07330dd47cc8c2e9b9f9f
Paul R Alexander <palexander@stanford.edu>
2014-08-06 10:31:15 -0700

    Lookups for arrays

---
+++ lib/resource_index/enumerable.rb
@@ -0,0 +1,60 @@
+module Enumerable
+  WRONG_TYPE_ERROR = "Using ResourceIndex monkeypatches requires all elements be of type ResourceIndex::Class"
+
+  ##
+  # Get back counts for a class per resource
+  # @resources [Array] Array of strings corresponding to requested resources
+  # @opts [Hash] Hash holding options
+  #   @opts[:expand] TrueClass|FalseClass
+  def ri_counts(*args)
+    raise ArgumentError, WRONG_TYPE_ERROR unless self.all? {|e| e.is_a?(ResourceIndex::Class)}
+    hashes = self.map {|c| c.xxhash}
+    resources, opts = ri_opts(args)
+    counts = {}
+    threads = []
+    resources.each do |res|
+      next unless res.populated?
+      threads << Thread.new do
+        counts[res.acronym] = res.concept_count(hashes, opts)
+      end
+    end
+    threads.each(&:join)
+    counts
+  end
+
+  ##
+  # Get back counts for a class per resource
+  # @resources [Array] Array of strings corresponding to requested resources
+  # @opts [Hash] Hash holding options
+  #   @opts[:expand] TrueClass|FalseClass
+  #   @opts[:size] Integer Size of the result set
+  #   @opts[:from] Integer Start at this result
+  def ri_docs(*args)
+    raise ArgumentError, WRONG_TYPE_ERROR unless self.all? {|e| e.is_a?(ResourceIndex::Class)}
+    hashes = self.map {|c| c.xxhash}
+    resources, opts = ri_opts(args)
+    docs = {}
+    threads = []
+    resources.each do |res|
+      next unless res.populated?
+      threads << Thread.new do
+        docs[res.acronym] = res.concept_docs(hashes, opts)
+      end
+    end
+    threads.each(&:join)
+    docs
+  end
+
+  private
+
+  def ri_opts(args)
+    opts = args.pop
+    if !opts.is_a?(Hash)
+      args.push(opts)
+      opts = {}
+    end
+    resources = args.map {|res| RI::Resource.find(res.to_s)}.compact
+    resources = ResourceIndex::Resource.all if resources.empty?
+    return resources, opts
+  end
+end
