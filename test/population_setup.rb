d69c55587c2c72dd1f2886daf22308fdca7e1be6
Paul R Alexander <palexander@stanford.edu>
2014-10-16 15:52:04 -0700

    Recreate mokeypatches for simplified test data

--- test/population_setup.rb
+++ test/population_setup.rb
@@ -1,34 +1,43 @@
 ##
-# Test data for use in populations so we don't need redis, mgrep, 4store, etc
-def gzip_read(path)
-  data = nil
-  Zlib::GzipReader.open(path) do |gz|
-    begin
-      data = Marshal.load(gz.read)
-    ensure
-      gz.close
+# Monkeypatch to find starting point for all occurrances of `needle` in string
+class ::String
+  def index_all(needle)
+    found = []
+    current_index = -1
+    while current_index = index(needle, current_index+1)
+      found << current_index
     end
+    found
   end
-  data
 end
-$test_converted = gzip_read(File.expand_path("../data/converted.dump.gz", __FILE__))
-$test_annotations = gzip_read(File.expand_path("../data/annotations.dump.gz", __FILE__))
-$test_annotation_counts = gzip_read(File.expand_path("../data/annotation_counts.dump.gz", __FILE__))
-$test_annotation_counts_anc = gzip_read(File.expand_path("../data/annotation_counts_anc.dump.gz", __FILE__))
-$test_ancestors = gzip_read(File.expand_path("../data/ancestors.dump.gz", __FILE__))
-$test_latest_sub = gzip_read(File.expand_path("../data/latest_sub.dump.gz", __FILE__))
 
 ##
 # Monkeypatched mock methods for use in populations so we don't need redis, mgrep, 4store, etc
 class RI::Population::LabelConverter
   def convert(mgrep_matches, annotations)
-    $test_converted[mgrep_matches]
+    classes = []
+    annotations.each do |a|
+      expanded = LABEL_ID_TO_CLASS_MAP[a.string_id.to_i]
+      expanded.each do |cls_data|
+        acronym, id = cls_data
+        classes << RI::Population::Class.new(id, acronym, a.value, a.string_id)
+      end
+    end
+    classes
   end
 end
 
 module MockMGREP
   def annotate(text,longword,wholeword=true)
-    $test_annotations[text]
+    matches = []
+    up_text = text.upcase
+    DICTIONARY.keys.each do |entry|
+      hits = up_text.index_all(entry)
+      hits.each do |hit|
+        matches << [DICTIONARY[entry].to_s, (hit+1).to_s, (hit+entry.length).to_s, entry]
+      end
+    end
+    return RI::Population::Mgrep::AnnotatedText.new(up_text, matches)
   end
 end
 
@@ -42,13 +51,6 @@ end
 
 class RI::Population::Class
   def retrieve_ancestors(acronym, submission_id)
-    $test_ancestors[self.xxhash]
+    XXHASH_TO_ANCESTOR_XXHASH[self.xxhash]
   end
 end
-
-class RI::Population::Manager
-  def latest_submissions_sparql
-    $test_latest_sub
-  end
-end
-
