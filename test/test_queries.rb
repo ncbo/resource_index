0a9fdb4fd3245cf0d5c7a0a2eeeb3d2f7cb1e6f8
Paul R Alexander <palexander@stanford.edu>
2014-10-16 17:39:01 -0700

    Fix query tests

--- test/test_queries.rb
+++ test/test_queries.rb
@@ -11,43 +11,43 @@ class RI::TestQueries < RI::TestCase
 
     ##
     # Test counts
-    DIRECT.each do |id|
-      assert_equal DIRECT_COUNT[id], res.concept_count(id), "direct count with id #{id} is bad"
+    CLASS_XXHASH.each do |id|
+      assert_equal DIRECT_ANNOTATION_COUNTS[id], res.concept_count(id), "direct count with id #{id} is bad"
     end
 
-    ANCESTOR.each do |id|
-      assert_equal ANCESTOR_COUNT[id], res.concept_count(id, expand: true), "ancestor count with id #{id} is bad"
+    XXHASH_TO_ANCESTOR_XXHASH.values.flatten.uniq.each do |id|
+      assert_equal ANCESTOR_ANNOTATION_COUNTS[id], res.concept_count(id, expand: true), "ancestor count with id #{id} is bad"
     end
 
     ##
     # Test returned documents
-    DIRECT.each do |id|
+    CLASS_XXHASH.each do |id|
       docs = res.concept_docs(id, size: 500).map {|d| d.id}.sort
-      assert_equal DIRECT_DOCS[id].sort, docs
+      assert_equal XXHASH_TO_DOCS[id].sort, docs
     end
 
-    ANCESTOR.each do |id|
+    XXHASH_TO_ANCESTOR_XXHASH.values.flatten.uniq.each do |id|
       docs = res.concept_docs(id, expand: true, size: 500).map {|d| d.id}.sort
-      assert_equal ANCESTOR_DOCS[id].sort, docs, "In local but not ES: #{ANCESTOR_DOCS[id].sort - docs}\nIn ES but not local: #{docs - ANCESTOR_DOCS[id].sort}"
+      assert_equal ANCESTOR_XXHASH_TO_DOCS[id].sort, docs, "In local but not ES: #{ANCESTOR_XXHASH_TO_DOCS[id].sort - docs}\nIn ES but not local: #{docs - ANCESTOR_XXHASH_TO_DOCS[id].sort}"
     end
 
     ##
     # Test multiple classes in a single doc (via AND)
-    hashes = [1514996459, 2466199616]
+    hashes = [560039333, 1829708204]
     docs = res.es_concept_docs(hashes)
     assert_equal 1, docs.length
-    assert_equal "E-GEOD-32422", docs.first["_id"]
+    assert_equal "2", docs.first["_id"]
 
     ##
     # Test multiple classes in a single doc (via OR)
-    hashes = [2466199616, 2631822857]
+    hashes = [3305416963, 921784164]
     docs = res.es_concept_docs(hashes, bool: :should)
-    assert_equal 3, docs.length
-    assert_equal ["E-GEOD-32422", "E-GEOD-40205", "E-BAIR-1"].sort, docs.map {|d| d["_id"]}.sort
+    assert_equal 4, docs.length
+    assert_equal ["1", "2", "3", "4"].sort, docs.map {|d| d["_id"]}.sort
 
     ##
     # Pass bad options to count query (should still work)
-    hashes = [1514996459, 2466199616]
+    hashes = [560039333, 1829708204]
     count = res.es_concept_count(hashes, size: 50, from: 0)
     assert_equal 1, count
   end
