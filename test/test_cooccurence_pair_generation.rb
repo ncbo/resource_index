ce911143bd5d9a780656fefb6fc8c053976d493b
Jennifer Vendetti <vendetti@stanford.edu>
2014-10-23 16:56:28 -0700

    Separate tests for label and class pairs.

--- test/test_cooccurence_pair_generation.rb
+++ test/test_cooccurence_pair_generation.rb
@@ -1,55 +1,69 @@
 require_relative 'test_case'
+require 'csv'
 
 class RI::TestCooccurencePairGeneration < RI::TestCase
 
-  @class_pairs = [
-    [1829708204, 560039333],
-    [1829708204, 921784164],
-    [2135716011, 1829708204],
-    [2135716011, 560039333],
-    [2135716011, 921784164],
-    [2135716011, 921784164],
-    [2135716011, 921784164],
-    [2957120221, 1829708204],
-    [2957120221, 2135716011],
-    [2957120221, 2135716011],
-    [2957120221, 2135716011],
-    [2957120221, 560039333],
-    [2957120221, 921784164],
-    [2957120221, 921784164],
-    [2957120221, 921784164],
-    [3305416963, 2135716011],
-    [3305416963, 2135716011],
-    [3305416963, 2957120221],
-    [3305416963, 2957120221],
-    [3305416963, 921784164],
-    [3305416963, 921784164],
-    [3305416963, 921784164],
-    [921784164, 560039333]
-  ]
+  def test_label_pair_generation
+    known_label_pairs = [
+      ["west", "sorcerer"],
+      ["west", "sorcerer"],
+      ["witch", "east"],
+      ["witch", "sorcerer"],
+      ["witch", "west"],
+      ["witch", "west"]
+    ]
 
-  @label_pairs = [
-    ["west", "sorcerer"],
-    ["west", "sorcerer"],
-    ["witch", "east"],
-    ["witch", "sorcerer"],
-    ["witch", "west"],
-    ["witch", "west"]
-  ]
-
-  def test_pair_generation
     res = RI::Resource.find("WITCH")
     mgrep = MockMGREPClient.new
-    populator = RI::Population::Manager.new(res, 
-    {
-      mgrep_client: mgrep, 
-      bulk_index_size: 500, 
-      write_label_pairs: true, 
-      write_class_pairs: true
-    })
-    @es = RI.es # triggers delete on teardown
-    @index_id = populator.populate()
+    populator = RI::Population::Manager.new(res, mgrep_client: mgrep, bulk_index_size: 500, write_label_pairs: true)
+    RI.es # triggers delete on teardown
+    index_id = populator.populate()
     sleep(2) # wait for indexing to complete
+
+    label_pairs_path = File.expand_path(File.join("cooccurence_results", "WITCH_labels", "#{index_id}" + ".tsv"))
+    assert File.file?(label_pairs_path)
+    assert_equal(known_label_pairs.size, File.foreach(label_pairs_path).count)
+
+    label_pairs = CSV.read(label_pairs_path, col_sep: "\t")
+    assert_equal(known_label_pairs, label_pairs.sort!)
+  end
+
+  def test_class_pair_generation
+    known_class_pairs = [
+      [1829708204, 560039333],
+      [1829708204, 921784164],
+      [2135716011, 1829708204],
+      [2135716011, 560039333],
+      [2135716011, 921784164],
+      [2135716011, 921784164],
+      [2135716011, 921784164],
+      [2957120221, 1829708204],
+      [2957120221, 2135716011],
+      [2957120221, 2135716011],
+      [2957120221, 2135716011],
+      [2957120221, 560039333],
+      [2957120221, 921784164],
+      [2957120221, 921784164],
+      [2957120221, 921784164],
+      [3305416963, 2135716011],
+      [3305416963, 2135716011],
+      [3305416963, 2957120221],
+      [3305416963, 2957120221],
+      [3305416963, 921784164],
+      [3305416963, 921784164],
+      [3305416963, 921784164],
+      [921784164, 560039333]
+    ]
+
+    res = RI::Resource.find("WITCH")
+    mgrep = MockMGREPClient.new
+    populator = RI::Population::Manager.new(res, mgrep_client: mgrep, bulk_index_size: 500, write_class_pairs: true)
+    RI.es # triggers delete on teardown
+    index_id = populator.populate()
+    sleep(2) # wait for indexing to complete
+
+    class_pairs_path = File.expand_path(File.join("cooccurence_results", "WITCH_classes", "#{index_id}" + ".tsv"))
+    assert File.file?(class_pairs_path)
   end
 
   def teardown
