5be3fe00b3b0f3a871ca5252907d3243ed7a9ca8
Jennifer Vendetti <vendetti@stanford.edu>
2014-10-29 16:15:13 -0700

    Update unit tests.
    Use newly available methods from RI::Population::Manager for
    determining the paths to cooccurrence output.

--- test/test_cooccurence_pair_generation.rb
+++ test/test_cooccurence_pair_generation.rb
@@ -3,8 +3,6 @@ require 'csv'
 
 class RI::TestCooccurencePairGeneration < RI::TestCase
 
-  COOCCURENCE_RESULTS_DIR = 'cooccurence_results'
-
   def test_label_pair_generation
     known_label_pairs = [
       ["west", "sorcerer"],
@@ -15,11 +13,10 @@ class RI::TestCooccurencePairGeneration < RI::TestCase
       ["witch", "west"]
     ]
 
-    index_id = populate(write_label_pairs: true)
-    label_pairs_path = File.expand_path(File.join(COOCCURENCE_RESULTS_DIR, "WITCH_labels", "#{index_id}" + ".tsv"))
+    mgr = populate(write_label_pairs: true)
+    label_pairs_path = mgr.label_pairs_path
     assert File.file?(label_pairs_path)
     assert_equal(known_label_pairs.size, File.foreach(label_pairs_path).count)
-
     label_pairs = CSV.read(label_pairs_path, col_sep: "\t")
     assert_equal(known_label_pairs.sort, label_pairs.sort)
   end
@@ -60,17 +57,18 @@ class RI::TestCooccurencePairGeneration < RI::TestCase
       ["921784164", "560039333"]   
     ]
 
-    index_id = populate(write_class_pairs: true)
-    class_pairs_path = File.expand_path(File.join(COOCCURENCE_RESULTS_DIR, "WITCH_classes", "#{index_id}" + ".tsv"))
+    mgr = populate(write_class_pairs: true)
+    class_pairs_path = mgr.class_pairs_path
     assert File.file?(class_pairs_path)
     assert_equal(known_class_pairs.size, File.foreach(class_pairs_path).count)
-
     class_pairs = CSV.read(class_pairs_path, col_sep: "\t")
     assert_equal(known_class_pairs.sort, class_pairs.sort)
   end
 
   def teardown
-    FileUtils.rm_rf(COOCCURENCE_RESULTS_DIR)
+    mgr = RI::Population::Manager.new(RI::Resource.find("WITCH"), mgrep_client: MockMGREPClient.new)
+    settings = mgr.settings
+    FileUtils.rm_rf(settings.cooccurrence_output)
   end
 
   private
@@ -81,16 +79,16 @@ class RI::TestCooccurencePairGeneration < RI::TestCase
 
     res = RI::Resource.find("WITCH")
     mgrep = MockMGREPClient.new
-    populator = RI::Population::Manager.new(res, {
+    mgr = RI::Population::Manager.new(res, {
       mgrep_client: mgrep,
       skip_es_storage: true,
       write_class_pairs: write_class_pairs,
       write_label_pairs: write_label_pairs
     })
     RI.es # triggers delete on teardown
-    index_id = populator.populate()
+    index_id = mgr.populate()
     sleep(2) # wait for indexing to complete
 
-    return index_id
+    return mgr
   end
 end
